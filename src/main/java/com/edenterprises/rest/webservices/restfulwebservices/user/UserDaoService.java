ad31e7a26841e3ab2a0e9c131d0738561686cf3e
JEdwards <jordanscottedwards@gmail.com>
2023-02-09 16:28:23 -0500

    Implemented save user API

--- src/main/java/com/edenterprises/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/edenterprises/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -10,20 +10,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
 	
+	private static int usersCount = 0;
+	
 	private static List<User> users = new ArrayList<>();
 	
 	static {
-		users.add(new User(1, "Jordan", LocalDate.now().minusYears(32)));
-		users.add(new User(2, "Izzy", LocalDate.now().minusYears(34)));
-		users.add(new User(3, "Steve", LocalDate.now().minusYears(31)));
+		users.add(new User(++usersCount, "Jordan", LocalDate.now().minusYears(32)));
+		users.add(new User(++usersCount, "Izzy", LocalDate.now().minusYears(34)));
+		users.add(new User(++usersCount, "Steve", LocalDate.now().minusYears(31)));
 	}
 	
 	public List<User> findAll() {
 		return users;
 	}
 	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+	
 	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
 	}
+	
 }
