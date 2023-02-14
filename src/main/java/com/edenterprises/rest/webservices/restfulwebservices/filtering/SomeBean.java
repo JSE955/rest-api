a058b2a0a87fe080aad4dea02ce6fbd3c9e39555
JEdwards <jordanscottedwards@gmail.com>
2023-02-14 10:21:38 -0500

    Implement dynamic filtering

--- src/main/java/com/edenterprises/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/edenterprises/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,13 +1,13 @@
 package com.edenterprises.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties("field1")
+//@JsonIgnoreProperties("field1")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
 	
-	@JsonIgnore
+	//@JsonIgnore
 	private String field2;
 	private String field3;
 
