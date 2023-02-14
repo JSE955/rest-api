e78bfc81f2c0ea577d1d2bbbbf6bcdc7638ec651
JEdwards <jordanscottedwards@gmail.com>
2023-02-14 10:35:43 -0500

    Created User entity and connected to H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER_DETAILS(id, birth_date, name)
+VALUES(10001, current_date(), 'Jordan');
+
+INSERT INTO USER_DETAILS(id, birth_date, name)
+VALUES(10002, current_date(), 'Izzy');
+
+INSERT INTO USER_DETAILS(id, birth_date, name)
+VALUES(10003, current_date(), 'Mazie');
